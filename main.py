72b3a221e6625387cc54ed40c5c0712fe0f1f5ce
Sultan-Omarov <129597612+Sultan-Omarov@users.noreply.github.com>
2023-04-29 14:47:31 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import time
+import random
+
+from bot_logic import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$ping'):
+        await message.channel.send('pong(Тест)')
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$help'):
+        await message.channel.send("Команды, которые доступны вам : $hello,$bye,$pass,$flipcoin")
+    elif message.content.startswith('$flipcoin'):
+        await message.channel.send(flip_coin())
+    else:
+        await message.channel.send('Я незнаю такую команду')
+
+client.run("MTEwMTc4MDM1MTMyMTM5NTI0MQ.GbF_8C.Sc7-zHItjZJ7n5BFy_9lxVSnUxLIaO4jsI1vjI")
